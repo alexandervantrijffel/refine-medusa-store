6d922243ae87ced54f7d9eb39cacf085627e2a19
Alexander van Trijffel <alexander@structura.ws>
2023-04-16 08:04:16 +0200

    "Initial commit from Create Refine App"

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    experimental: {
+        newNextLinkBehavior: true,
+    },
+    images: {
+        loader: "imgix",
+        path: "https://refine-store.imgix.net/",
+    },
+};
